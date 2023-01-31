939f1b5ac67244c99b634bfcdff0f39ff6b056b7
tony-nguyen1 <nguyentoony@gmail.com>
2023-01-31 14:07:10 +0100

    un peu de style

--- src/app/mock-lang.ts
+++ src/app/mock-lang.ts
@@ -5,6 +5,6 @@ export const LANG: Lang[] = [
     { nom: "Java", surnom: "java", logo: '../../assets/logo/JavaLogo.svg' },
     { nom: "Syntactically Awesome Style Sheets", surnom: "SCSS", logo: '../../assets/logo/sass.svg' },
     { nom: "TypeScript", surnom: "ts", logo: '../../assets/logo/typescript.svg' },
-    { nom: "Angular", surnom: "angular", logo: '../../assets/logo/python.svg' },
+    { nom: "Angular", surnom: "angular", logo: '../../assets/logo/angular.svg' },
 
 ];
